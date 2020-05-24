1268707f92b4a0112468dba3f56e408bfb9b309c
Student_GA113 <Student_GA113@Student-GA113.local>
2020-05-24 03:15:46 -0400

    add app.js code

--- calorie_tracker-app/src/App.js
+++ calorie_tracker-app/src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import React, {Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    isLoading: true,
+    calories: []
+  };
+
+  async componentDidMount() {
+    const response = await fetch('/raf_api/v1/calories');
+    const body = await response.json();
+    this.setState({ calories: body, isLoading: false });
+  }
+
+  render() {
+    const {calories, isLoading} = this.state;
+
+    if (isLoading) {
+      return <p>Loading...</p>;
+    }
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <div className="App-intro">
+            <h2>Calorie  Counter</h2>
+            {calories.map(calorie =>
+              <div key={calorie.id}>
+                {calorie.calories}cal
+                <br></br>
+                {calorie.fats}g
+                <br></br>
+                {calorie.protein}g
+              </div>
+            )}
+          </div>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
