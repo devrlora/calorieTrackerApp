47ec89f3e7777dc0c61bbc405507b1be1d9e9d49
Student_GA113 <Student_GA113@Student-GA113.local>
2020-05-25 16:29:05 -0400

    add components folder

---
+++ calorie_tracker-app/src/components/Totals.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Totals() {
+    return (
+        <div>
+            
+            <form>
+            <label>
+            Totals:
+                <input type='text' name = 'name'/>
+                <input type='text' name = 'name'/>
+                <input type='text' name = 'name'/>
+            </label>
+            <button>Add</button>
+        </form>
+        </div>
+    )
+}
