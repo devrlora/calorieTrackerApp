14257cffec9369971e1aa5b79408338ad59a7931
Student_GA113 <Student_GA113@Student-GA113.local>
2020-05-25 16:50:29 -0400

    add Addcalories component

---
+++ calorie_tracker-app/src/components/AddCalories.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+class AddCalories extends Component {
+    constructor(props){
+        super(props);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+onSubmit(event){
+    event.preventDefault();
+   this.props.onAdd(this.idInput.value,this.firstNameInput.value,this.lastNameInput.value,this.proteinsInput.value)
+   this.idInput.value = "";
+   this.caloriesInput.value = "";
+   this.lastNameInput.value = "";
+   this.proteinsInput.value = "";
+
+}
+
+render(){
+    return (
+        <form onSubmit={this.onSubmit}>
+            <h2>Add Calories</h2>
+            <input placeholder = "Id" ref={idInput=>this.idInput = idInput}/>
+            <input placeholder = "Calories" ref={caloriesInput=>this.caloriesInput = caloriesInput}/>
+            <input placeholder = "Fats"ref={fatsInput => this.fatsInput = fatsInput} />
+            <input placeholder = "Proteins"ref={proteinsInput => this.proteinsInput = proteinsInput} />
+            <button>Add</button>
+        </form>
+    )
+  }
+}
+
+export default AddCalories;
